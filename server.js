486ddec7c417eda673ecc3c02401c55475f2c4b8
Mark Estrada <mark.estrada.dev>
2022-08-05 19:25:57 -0500

    deploy fixes

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const publicPath = path.join(__dirname, './public');
+const publicPath = path.join(__dirname, 'public');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
