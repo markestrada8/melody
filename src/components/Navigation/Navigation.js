7f85817f2b1dc4b12e622fe2574c3912cf941b3f
Mark Estrada <mark.estrada.dev>
2022-08-09 17:10:24 -0500

    merge

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import { GiMusicSpell } from 'react-icons/gi';
 import { FaBars, FaTimes } from 'react-icons/fa';
